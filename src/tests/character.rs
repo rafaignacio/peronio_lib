17ee6b5b4ac0686cf9b28e2b40fc4983c984d76e
Rafael Ignacio Pinto <ignacio.rafael@gmail.com>
2024-04-05 23:04:39 +0200

    improvin test

--- src/tests/character.rs
+++ src/tests/character.rs
@@ -1,2 +1,13 @@
+use crate::character::{Character, CharacterErrors};
+
+use super::MockedCharService;
+
 #[test]
-fn should_create_char() {}
+fn should_create_char() -> Result<(), CharacterErrors> {
+    let service = MockedCharService {
+        name_should_exist: false,
+    };
+    let _ = Character::new(service)?;
+
+    Ok(())
+}
